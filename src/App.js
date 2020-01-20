61d3b3f13eef2a4717f594e33edabcafa6144312
stevekutz <stkutz@gmail.com>
2020-01-19 20:19:43 -0600

    added Semantic UI right dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, {useState} from "react";
 import World from "./features/world";
+
 import AboutTemp from './about_temp/AboutTemp'
+import LandingTemp from './landing_temp/LandingTemp';
+
+import {Route, NavLink, Switch} from 'react-router-dom';
+import {Dropdown, Menu, Segment} from 'semantic-ui-react';
+import './nav.css';
 
 const App = () => {
 
@@ -8,13 +14,86 @@ const App = () => {
   
     return (
         <div>
-      
-  
-            <World />
-            <AboutTemp/>
+            
+            <Menu>
+                <Menu.Item>
+                    <NavLink to = '/' exact> Home </NavLink> 
+                </Menu.Item>
+                <Menu.Item>
+                    <NavLink to = '/about' exact> About </NavLink> 
+                </Menu.Item>
+                <Menu.Item>
+                    <NavLink to = '/Login' exact> Login </NavLink>
+                </Menu.Item>
+                <Menu.Item>
+                    <NavLink to = '/world'> World </NavLink>   
+                </Menu.Item>
+        
+                
+                
+                <Menu.Menu position = 'right'>
+                <Dropdown>
+                <Dropdown.Menu>
+                <Dropdown.Item>
+                <NavLink to = '/' exact> Home </NavLink> 
+                </Dropdown.Item>
+                <Dropdown.Item>
+                <NavLink to = '/about' exact> About </NavLink>
+                </Dropdown.Item>
+                <Dropdown.Item>
+                <NavLink to = '/world'> World </NavLink>   
+                </Dropdown.Item>
+                
+                </Dropdown.Menu>
+                {/*
+                    <div style = {{display: 'flex', zIndex: '0', flexDirection: 'column'}}>
+                    <NavLink to = '/login' exact> Login </NavLink>  
+                    </div>
+                */}                                    
+                </Dropdown>
+                </Menu.Menu>
+            </Menu>
+            
+
+
+                
+            <Route
+                exact path = '/' 
+                render = { (props) => <LandingTemp {...props} landingProp = {` the Landing Page`} />}
+            />
+
+            <Route
+                exact path = '/about' 
+                render = { (props) => 
+                <AboutTemp 
+                    {...props} 
+                    aboutProp = {` the About Page`} 
+                />}
+            />
+        
+        {/*    
+            <Route
+                exact path = 'login' 
+                render = { (props) => 
+                    <Login 
+                    {...props} 
+
+                    LoginProp = {` the Login Page`} 
+                    />}
+            />
+        */}            
+            <Route
+                exact path = '/world' 
+                render = { (props) => 
+                <World 
+                    {...props} 
+                    worldProp = {` the World Page`} 
+                />}
+            />
+             
     
     
-      </div>
+        </div>
   )
 }
 
