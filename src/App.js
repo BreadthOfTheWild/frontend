10ab37dfcffd3d98d4b1385c8b8ca23fda081758
stevekutz <stkutz@gmail.com>
2020-01-20 13:24:10 -0600

    moved navigration into NavBar component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, {useState} from "react";
 import World from "./features/world";
 
-import AboutTemp from './about_temp/AboutTemp'
+import NavBar from './nav/NavBar';
+import AboutTemp from './about_temp/AboutTemp';
 import LandingTemp from './landing_temp/LandingTemp';
 
 import {Route, NavLink, Switch} from 'react-router-dom';
@@ -14,82 +15,8 @@ const App = () => {
   
     return (
         <div>
-            
-            <Menu>
-                <Menu.Item>
-                    <NavLink to = '/' exact> Home </NavLink> 
-                </Menu.Item>
-                <Menu.Item>
-                    <NavLink to = '/about' exact> About </NavLink> 
-                </Menu.Item>
-                <Menu.Item>
-                    <NavLink to = '/Login' exact> Login </NavLink>
-                </Menu.Item>
-                <Menu.Item>
-                    <NavLink to = '/world'> World </NavLink>   
-                </Menu.Item>
-        
-                
-                
-                <Menu.Menu position = 'right'>
-                <Dropdown>
-                <Dropdown.Menu>
-                <Dropdown.Item>
-                <NavLink to = '/' exact> Home </NavLink> 
-                </Dropdown.Item>
-                <Dropdown.Item>
-                <NavLink to = '/about' exact> About </NavLink>
-                </Dropdown.Item>
-                <Dropdown.Item>
-                <NavLink to = '/world'> World </NavLink>   
-                </Dropdown.Item>
-                
-                </Dropdown.Menu>
-                {/*
-                    <div style = {{display: 'flex', zIndex: '0', flexDirection: 'column'}}>
-                    <NavLink to = '/login' exact> Login </NavLink>  
-                    </div>
-                */}                                    
-                </Dropdown>
-                </Menu.Menu>
-            </Menu>
-            
+            <NavBar/>
 
-
-                
-            <Route
-                exact path = '/' 
-                render = { (props) => <LandingTemp {...props} landingProp = {` the Landing Page`} />}
-            />
-
-            <Route
-                exact path = '/about' 
-                render = { (props) => 
-                <AboutTemp 
-                    {...props} 
-                    aboutProp = {` the About Page`} 
-                />}
-            />
-        
-        {/*    
-            <Route
-                exact path = 'login' 
-                render = { (props) => 
-                    <Login 
-                    {...props} 
-
-                    LoginProp = {` the Login Page`} 
-                    />}
-            />
-        */}            
-            <Route
-                exact path = '/world' 
-                render = { (props) => 
-                <World 
-                    {...props} 
-                    worldProp = {` the World Page`} 
-                />}
-            />
              
     
     
@@ -98,3 +25,4 @@ const App = () => {
 }
 
 export default App;
+
---
+++ src/nav/NavBar.js
@@ -0,0 +1,100 @@
+import React, {useState} from "react";
+import World from "./features/world";
+
+import AboutTemp from './about_temp/AboutTemp'
+import LandingTemp from './landing_temp/LandingTemp';
+
+import {Route, NavLink, Switch} from 'react-router-dom';
+import {Dropdown, Menu, Segment} from 'semantic-ui-react';
+// import './nav.css';
+
+
+const NavBar = () => {
+
+    const [isActive, setIsActive] = useState(false, "isActive State");
+
+    return (
+        <Segment>
+            <Menu>
+            <Menu.Item>
+                <NavLink to = '/' exact> Home </NavLink> 
+            </Menu.Item>
+            <Menu.Item>
+                <NavLink to = '/about' exact> About </NavLink> 
+            </Menu.Item>
+            <Menu.Item>
+                <NavLink to = '/Login' exact> Login </NavLink>
+            </Menu.Item>
+            <Menu.Item>
+                <NavLink to = '/world'> World </NavLink>   
+            </Menu.Item>
+
+        
+        
+            <Menu.Menu position = 'right'>
+                <Dropdown>
+                    <Dropdown.Menu>
+                        <Dropdown.Item>
+                            <NavLink to = '/' exact> Home </NavLink> 
+                        </Dropdown.Item>
+                    <Dropdown.Item>
+                        <NavLink to = '/about' exact> About </NavLink>
+                    </Dropdown.Item>
+                    <Dropdown.Item>
+                        <NavLink to = '/world'> World </NavLink>   
+                    </Dropdown.Item>
+                
+                    </Dropdown.Menu>
+                {/*
+                    <div style = {{display: 'flex', zIndex: '0', flexDirection: 'column'}}>
+                    <NavLink to = '/login' exact> Login </NavLink>  
+                    </div>
+                */}                                    
+                </Dropdown>
+            </Menu.Menu>
+        </Menu>
+    
+
+
+        
+        <Route
+            exact path = '/' 
+            render = { (props) => <LandingTemp {...props} landingProp = {` the Landing Page`} />}
+        />
+
+        <Route
+            exact path = '/about' 
+            render = { (props) => 
+            <AboutTemp 
+                {...props} 
+                aboutProp = {` the About Page`} 
+            />}
+        />
+
+    {/*    
+        <Route
+            exact path = 'login' 
+            render = { (props) => 
+                <Login 
+                {...props} 
+
+                LoginProp = {` the Login Page`} 
+                />}
+        />
+    */}            
+        <Route
+            exact path = '/world' 
+            render = { (props) => 
+            <World 
+                {...props} 
+                worldProp = {` the World Page`} 
+            />}
+        />
+        </Segment>
+    )
+
+
+
+}
+
+export default NavBar;
